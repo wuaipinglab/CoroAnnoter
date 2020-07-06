1128590730f79582718579fa008a4c4539bfb871
mosmoon <yuren95@163.com>
2020-07-06 16:06:27 +0800

    frist

--- R/plot_protein_single.R
+++ R/plot_protein_single.R
@@ -24,15 +24,48 @@ plot_protein_single <-function(anno_R,TRSlocation) {
       theme(axis.ticks = element_blank(), axis.text.y = element_blank()) +
       theme(panel.border = element_blank())
     ####add protein
-    p <- p + geom_rect(aes(xmin = start, xmax = end,ymin = 0.9, ymax = 1.1,fill = stitle),
-                       data = strain,show.legend = TRUE)+
-      geom_label(aes(x = start + (end -start)/2, y = 1.22, label = stitle),
-                 data = strain,size = 8)
+    ####Layered
+    strain$order <- 1
+    #i <- 6
+    for(i in 2:nrow(strain)) {
+      temp.location <- c(strain$end[1:(i-1)],strain$start[i])
+      temp.location <- sort(temp.location)
+      dif <- i- which(temp.location==strain$start[i])
+      strain$order[i] <- 1+dif
+    }
+    ####
+    order.l <- strain$order[!duplicated(strain$order)]
+    if (any(order.l %in% 1)) {
+      p <- p + geom_rect(aes(xmin = start, xmax = end,ymin = 0.9, ymax = 1.1,fill = orf),
+                         data = strain[strain$order==1,],show.legend = TRUE)+
+        geom_text(aes(x = start + (end -start)/2, y = 1, label = orf),
+                   data = strain[strain$order==1,],size = 8)
+
+    }
+
+    if (any(order.l %in% 2)) {
+      p <- p + geom_rect(aes(xmin = start, xmax = end,ymin = 0.65, ymax = 0.85,fill = orf),
+                         data = strain[strain$order==2,],show.legend = TRUE)+
+        geom_text(aes(x = start + (end -start)/2, y = 0.75, label = orf),
+                   data = strain[strain$order==2,],size = 8)
+
+    }
+    if (any(order.l %in% 3)) {
+      p <- p + geom_rect(aes(xmin = start, xmax = end,ymin = 0.4, ymax = 0.6,fill = orf),
+                         data = strain[strain$order==3,],show.legend = TRUE)+
+        geom_text(aes(x = start + (end -start)/2, y = 0.5, label = orf),
+                  data = strain[strain$order==3,],size = 8)
+
+    }
+
     ####add TRS
     trs <- read.csv(TRSlocation)
-    p <- p + geom_point(data = trs,aes(x = start, y = 0.85), shape = 21,
-                        colour = "black", fill = "black", size = 6, show.legend = FALSE)
-    p <- p+geom_text(data = trs,aes(x=start + (end -start)/2,y=0.85,label=score),colour="white")
+    max.order <- max(order.l)
+
+    p <- p + geom_point(data = trs,aes(x = start, y = 1.1-0.25*max.order), shape = 21,
+                        colour = "NA", fill = "black", size = 6, show.legend = FALSE)+
+            geom_text(data = trs,aes(x=start + (end -start)/2,y=1.1-0.25*max.order,
+                                     label=score),colour="white")
     p <- p+labs(title = savename)
     plot(p)
   dev.off()
